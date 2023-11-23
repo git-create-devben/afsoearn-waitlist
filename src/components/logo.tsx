1cb251a5d4dfeda6c279e4cf185868b135af803e
git-create-devben <DevBen.tech@outlook.com>
2023-11-23 18:30:15 +0100

    add next image

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,8 +1,12 @@
+import Logos from "../../public/images/logo 2 (1).png";
+import Image from "next/image";
 export default function Logo() {
   return (
-    <div className="text-sm lg:text-lg">
-      AFSOEARN...<small className="text-pcolor text-md">....</small>
+    <div className="text-sm lg:text-lg flex items-center ">
+      <Image src={Logos} alt="logo" width={50} height={25} />
+      <p>
+        AFSOEARN...<small className="text-pcolor text-md">....</small>
+      </p>
     </div>
   );
-};
-
+}
