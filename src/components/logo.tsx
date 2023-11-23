b727cfd775a3a0f6f16511592487771c69f4719a
git-create-devben <DevBen.tech@outlook.com>
2023-11-23 17:06:13 +0100

    update

---
+++ src/components/logo.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// type Props = {}
+
+const Logo = () => {
+  return (
+    <div className="text-sm lg:text-lg">
+      AFSOEARN...<small>....</small>
+    </div>
+  );
+};
+
+export default Logo;
---
+++ src/components/navbar.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import Logo from "@/components/logo";
+
+// type Props = {}
+
+const navbar = () => {
+  return (
+    <div className="p-6">
+      <nav className="flex justify-between">
+        <div className="flex gap-2 lg:justify-between lg:gap-60">
+          <Logo />
+
+          <p className="text-md hidden lg:md ">
+            create<small>...</small>inspire<small>...</small>earn
+          </p>
+
+          {/* <p className="text-md lg:md sm:hidden ">
+
+            create<small>...</small>inspire<small>...</small>earn
+
+          </p> */}
+        </div>
+
+        <ul className="flex gap-2 text-sm lg:text-lg lg:justify-between lg:gap-20 lg:mr-20">
+          <li>
+            <small>.</small>About Us
+          </li>
+
+          <li>
+            <small>.</small>Mission
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default navbar;
