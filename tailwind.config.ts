badf65d460269e7d917d74c9340f4649e084f35f
git-create-devben <DevBen.tech@outlook.com>
2023-11-23 17:15:07 +0100

    fixed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,9 @@ const config: Config = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    color:{
+      'pcolor': "#FAB400"
+    },
   },
   plugins: [],
 }
