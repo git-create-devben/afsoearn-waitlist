480d5c35e7d6b78c941d64024a81de3e7f6cb0a2
git-create-devben <DevBen.tech@outlook.com>
2023-11-23 22:45:20 +0100

    adjust

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -10,8 +10,8 @@ const Header = () => {
       <section>
         <div className="grid lg:grid-cols-2 ">
           <div className="relative">
-            <div className="grid grid-cols-2 space-x-80 ml-10 ">
-              <Image src={Frame} alt="frame" width={150} height={50} />
+            <div className="grid grid-cols-2 space-x-[30rem] ml-10 ">
+              <Image src={Frame} alt="frame" width={150} height={50} className="ml-60"/>
               <Image
                 src={Love}
                 alt="frame"
@@ -22,20 +22,20 @@ const Header = () => {
               <Image
                 src={Circle}
                 alt="frame"
-                width={150}
+                width={200}
                 height={50}
-                className="mt-40 ml-20"
+                className="mt-[6rem] ml-20"
               />
             </div>
-            <div className="space-y-2 absolute mt-[-14rem] ml-16">
-              <h1 className="lg:text-[5.4rem] leading-tight text-bold text-4xl">
+            <div className="space-y-2 absolute mt-[-12rem] ml-16">
+              <h1 className="lg:text-[5rem] leading-tight text-bold text-4xl">
                 WELCOME YOUR <br />
                 <span className="text-pcolor">HIGHNESS</span>
               </h1>
               <p>we have been waiting for you</p>
             </div>
           </div>
-          <div className="lg:ml-10 relative">
+          <div className="lg:ml-60 relative">
             <Image
               src={Screen}
               alt="frame"
@@ -46,7 +46,7 @@ const Header = () => {
             <div></div>
           </div>
         </div>
-        <section className=" flex justify-center mt-10 lg:mt-40">
+        <section>
           <Hero />
         </section>
       </section>
