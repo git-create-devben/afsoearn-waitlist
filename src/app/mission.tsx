afc62508359bb58062019c759c2ecfc4f4005eb0
git-create-devben <DevBen.tech@outlook.com>
2023-11-24 10:45:01 +0100

    import images

--- src/app/mission.tsx
+++ src/app/mission.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image"
-
+import rectangle1 from "../../public/images/Rectangle (1).png"
+import rectangle2 from "../../public/images/Rectangle (2).png"
+import rectangle3 from "../../public/images/Rectangle.png"
 export default function Mission(){
     return(
         <main className="flex flex-col p-10">
