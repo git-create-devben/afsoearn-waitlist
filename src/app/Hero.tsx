2eaeb09a75dabf3fa5ec2cc643635b33c3fdb3bf
git-create-devben <DevBen.tech@outlook.com>
2023-11-24 10:31:39 +0100

    adjust design

--- src/app/Hero.tsx
+++ src/app/Hero.tsx
@@ -7,7 +7,7 @@ import emoji3 from "../../public/images/emoji3.png"
 export default function Hero() {
   return (
     <main className="p-10 w-full">
-      <div className="bg-scolor p-4  mb-20 lg:w-[50%] m-auto rounded-md">
+      <div className="bg-scolor p-4  mb-20 lg:w-[50%] m-auto rounded-md text-center">
         <h1 className="text-4xl">
           <span className="text-pcolor">WAIT</span>....lets do a quick reality
           check on you
@@ -16,15 +16,17 @@ export default function Hero() {
       <div className="grid lg:grid-cols-2 p-2 place-content-around place-items-center gap-4">
         <div className="relative bg-scolor p-4  rounded-full gap-4 place-self-center">
           <p className="relative flex items-center">You have different social media account <span className="absolute -right-12"><Mark/></span></p> 
-          <Image src={emoji1} alt="emoji" width={100} height={100} />
+          <Image src={emoji3} alt="emoji" width={50} height={10} className="absolute"/>
         </div>
         <div className="bg-scolor p-4 w- rounded-full place-self-center">
         <p className="relative flex items-center">You have different social media account <span className="absolute -right-12"><Mark/></span></p> 
+        <Image src={emoji2} alt="emoji" width={50} height={10} className="absolute"/>
         </div>
        
       </div>
       <div className="bg-scolor p-4  mb-20 lg:w-[22.5%] m-auto rounded-full mt-20">
       <p className="relative flex items-center">You have different social media account <span className="absolute -right-12"><Mark/></span></p> 
+      <Image src={emoji1} alt="emoji" width={50} height={10} className="absolute"/>
         </div>
 
       <div className="text-center lg:text-4xl leading-loose mt-10 lg:mt-40 text-2xl">
